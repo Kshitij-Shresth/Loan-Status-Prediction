b7f136f94063ad31e6b7469b854e8ae921af2006
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-17 10:24:21 +0300

    Create imports.py
    Collection of all required imports for the project

---
+++ imports.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import seaborn as sns
+import numpy as np 
